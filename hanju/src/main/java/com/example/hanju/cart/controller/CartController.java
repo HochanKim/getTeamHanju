be1c822f5224587a597a25d9b81b2da3f001c5a4
nihilism011 <nihilism011@gmail.com>
2024-09-23 18:10:09 +0900

    2024.09.21 아마 장바구니 완료

--- hanju/src/main/java/com/example/hanju/cart/controller/CartController.java
+++ hanju/src/main/java/com/example/hanju/cart/controller/CartController.java
@@ -1,20 +1,26 @@
 package com.example.hanju.cart.controller;
 
 import com.google.gson.Gson;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Controller
 public class CartController {
+    @Autowired
+    HttpSession session;
 
     @RequestMapping("cart/viewCart.do")
     public String cartMainPage(Model model) throws Exception{
+        model.addAttribute("userId",session.getAttribute("sessionId"));
         return "cart/viewCart";
     }
 
