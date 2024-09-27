dbee5e558097f3cfea4cb5d26fa9fa287d78b0a3
cleanMirror <clean_mirror@naver.com>
2024-09-27 18:11:44 +0900

    240927 관리자 페이지 유저 수정 작성중

--- hanju/src/main/java/com/example/hanju/admin/controller/AdminPageController.java
+++ hanju/src/main/java/com/example/hanju/admin/controller/AdminPageController.java
@@ -1,11 +1,17 @@
 package com.example.hanju.admin.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.hanju.admin.service.AdminPageService;
+import com.google.gson.Gson;
 
 @Controller
 public class AdminPageController {
@@ -18,4 +24,8 @@ public class AdminPageController {
         return "/admin/adminMain";
     }
 	
+	@RequestMapping("/admin/modifyUser.do")
+    public String modifyUser(Model model) throws Exception {
+        return "/admin/modifyUser";
+    }
 }
