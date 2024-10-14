79ff6d05c18dc731e055f6f9a201baeea4b5611d
장병철 <clean_mirror@naver.com>
2024-10-14 09:48:43 +0900

    DB백업

---
+++ DB백업/시퀀스 백업.sql
@@ -0,0 +1,43 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 월요일-10월-14-2024   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence BILL_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."BILL_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 168 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CART_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."CART_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 90 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence FUNDING_OPTION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."FUNDING_OPTION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence IMAGE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."IMAGE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 432 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence MAIN_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."MAIN_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 202 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence STORE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."STORE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 4 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence TEST_IMG_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."TEST_IMG_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence TEST_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."TEST_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 132 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
---
+++ DB백업/트리거 백업.sql
@@ -0,0 +1,47 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 월요일-10월-14-2024   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Trigger PIKUP_COUNT_TR
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "SYSTEM"."PIKUP_COUNT_TR" 
+AFTER INSERT OR UPDATE ON "HANJU_PICKUP_STOCK"
+FOR EACH ROW
+DECLARE
+    DIFF_COUNT NUMBER;
+BEGIN
+    IF INSERTING THEN
+        DIFF_COUNT := :NEW.STOCK;
+    ELSIF UPDATING THEN
+        DIFF_COUNT := :NEW.STOCK - :OLD.STOCK;
+    END IF;
+
+    UPDATE HANJU_PRODUCT H
+    SET H.STOCK = H.STOCK + DIFF_COUNT
+    WHERE H.PRODUCT_ID = :NEW.PRODUCT_ID;
+END;
+/
+ALTER TRIGGER "SYSTEM"."PIKUP_COUNT_TR" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TEST_COUNT_TR
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "SYSTEM"."TEST_COUNT_TR" 
+AFTER INSERT OR UPDATE ON "HANJU_PICKUP_STOCK"
+FOR EACH ROW
+DECLARE
+    DIFF_COUNT NUMBER;
+BEGIN
+    IF INSERTING THEN
+        DIFF_COUNT := :NEW.STOCK;
+    ELSIF UPDATING THEN
+        DIFF_COUNT := :NEW.STOCK - :OLD.STOCK;
+    END IF;
+
+    UPDATE HANJU_PRODUCT H
+    SET H.STOCK = H.STOCK + DIFF_COUNT
+    WHERE H.PRODUCT_ID = :NEW.PRODUCT_ID;
+END;
+/
+ALTER TRIGGER "SYSTEM"."TEST_COUNT_TR" ENABLE;
